fc726ac945d8a9f2a42aed1ac31fbf2758898af9
fobos78 <57541130+fobos78@users.noreply.github.com>
2021-05-03 20:04:14 +0300

    test-2

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+const { json } = require('express')
 const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send({message:'Hello World!'})
 })
 
 app.listen(port, () => {
