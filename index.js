e42a8887f5c098123d872ed97c54e39b8956c6e4
fobos78 <57541130+fobos78@users.noreply.github.com>
2021-05-03 23:37:20 +0300

    test-4

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const express = require('express');
 
 const app = express()
 const port = process.env.PORT || 3000;
-console.log('process.env.PORT-',process.env);
+console.log('process.env.PORT-',process.env.PORT);
 
 app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send({message:'Hello World!'})
-})
+});
+app.post('/', (req, res) => {
+    res.send(req.body)
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
