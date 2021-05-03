d4720e71f12feeed07018bff25d876ec63079022
fobos78 <57541130+fobos78@users.noreply.github.com>
2021-05-03 23:47:42 +0300

    test-6

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ console.log('process.env.PORT-',process.env.PORT);
 app.use(express.json())
 
 app.get('/', (req, res) => {
+    console.log(req);
   res.send({message:'Hello World!'})
 });
 app.post('/', (req, res) => {
-    res.send(req.body)
+    console.log(req.body);
+    res.send(req.body);
   })
 
 app.listen(port, () => {
